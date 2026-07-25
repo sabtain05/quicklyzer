c2b32ebf9710fe7e276f1a41e12f2557bfa5fd72
Sabtain Ali <sabtainalipk144@gmail.com>
2026-07-25 11:29:49 +0500

    fix: format code in detectHeavyFiles function for consistency

--- src/services/performance.ts
+++ src/services/performance.ts
@@ -19,5 +19,5 @@ export interface PerformanceAnalysis{
 
 
 function detectHeavyFiles(modules: ModuleInfo[]){
-    return modules.map(module=>({file:module.file, size: statSync(module.file).size}))
+    return modules.map(module=>({file: module.file, size: statSync(module.file).size}))
 }
