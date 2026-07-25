6cd927108063047dcf17fdc96abf59aa579767a3
Sabtain Ali <sabtainalipk144@gmail.com>
2026-07-25 15:52:49 +0500

    added build module complexity function

--- src/services/performance.ts
+++ src/services/performance.ts
@@ -104,7 +104,7 @@ function calculateImportDensity(modules: ModuleInfo[]){
 
 
 function buildModuleComplexity(modules: ModuleInfo[]){
-    return modules.map(module=>({file:module.file, score:module.imports.length})).sort((a,b)=>b.score-a.score)
+    return modules.map(module=>({file:module.file, score:module.imports.length})).sort((a,b)=>b.score-a.score).slice(0,5);
 }
 
 
