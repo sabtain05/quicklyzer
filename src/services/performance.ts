c8cb65d95480eef4abbc9c286f6c991fed6bf5b8
Sabtain Ali <sabtainalipk144@gmail.com>
2026-07-25 11:32:08 +0500

    fix: add newline at end of file in performance.ts

--- src/services/performance.ts
+++ src/services/performance.ts
@@ -21,3 +21,5 @@ export interface PerformanceAnalysis{
 function detectHeavyFiles(modules: ModuleInfo[]){
     return modules.map(module=>({file: module.file, size: statSync(module.file).size})).sort((a,b)=>b.size-a.size).slice(0,5);
 }
+
+
