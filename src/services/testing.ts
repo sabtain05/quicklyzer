c5015bddbdd3a4c4148e00b55cbfdb66edfe941b
Sabtain Ali <sabtainalipk144@gmail.com>
2026-07-27 12:16:45 +0500

    adding walk function

--- src/services/testing.ts
+++ src/services/testing.ts
@@ -29,5 +29,9 @@ function walk(dir: string, files: string[]){
         const full = join (dir, entry);
 
         const stats = statSync(full);
+
+        if(stats.isDirectory()){
+            
+        }
     }
 }
