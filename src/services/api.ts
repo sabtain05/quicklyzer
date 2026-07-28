866d9e1cf06e6e2a6e0e94e2c44dc801395a7093
Sabtain Ali <sabtainalipk144@gmail.com>
2026-07-28 17:31:07 +0500

    updating buildMethodStatistics function

--- src/services/api.ts
+++ src/services/api.ts
@@ -91,6 +91,8 @@ function discoverEndpoints(content: string, file: string){
 function buildMethodStatistics(endpoints: Endpoint[]){
     return{
         GET: endpoints.filter(e=>e.method==="GET").length,
+
+        POST: endpoints.filter(e=>e.method==="POST").length,
     }
 }
 
