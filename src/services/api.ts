b4a593b37f21467d25da8843844885e7be04dfc0
Sabtain Ali <sabtainalipk144@gmail.com>
2026-07-30 10:20:55 +0500

    removing unused variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -160,10 +160,6 @@ export function analyzeApi(projectPath: string, packageJson: any): ApiAnalysis{
 
     const swagger = "swagger-ui-express" in dependencies || "swagger-jsdoc" in dependencies || "@nestjs/swagger" in dependencies;
 
-    let middleware = 0;
-
-    let version="Unknown";
-
     const totalEndpoints = endpoints.length;
 
     const methods = buildMethodStatistics(endpoints);
