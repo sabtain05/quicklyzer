2f25b2b064d96955b4b4f76cacba89add3cc7560
Sabtain Ali <sabtainalipk144@gmail.com>
2026-07-31 11:18:36 +0500

    adding source maps in build

--- src/commands/scan.ts
+++ src/commands/scan.ts
@@ -269,6 +269,7 @@ export function scanCommand() {
     console.log(`Build System    : ${project.build.system}`);
     console.log(`Output Folders  : ${project.build.system}`);
     console.log(`Assets          : ${project.build.assets}`);
+    console.log(`Source Maps     : ${project.build.sourceMaps}`);
 
 
 
