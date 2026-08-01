dab64ab9fb9b2131d6aefde44b6cd07c046e1ba0
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-01 14:48:17 +0500

    Chore: release 0.9.0

--- src/commands/scan.ts
+++ src/commands/scan.ts
@@ -698,8 +698,8 @@ export function scanCommand() {
 
 
     title(".gitignore");
-    console.log(`Exists             : ${project.gitAnalysis.gitignore.exists ? "Yes" : "No"}`);
-    console.log(`Rules              : ${project.gitAnalysis.gitignore.rules}`);
+    console.log(`Exists         : ${project.gitAnalysis.gitignore.exists ? "Yes" : "No"}`);
+    console.log(`Rules          : ${project.gitAnalysis.gitignore.rules}`);
 
 
     title("Git Recommendations");
