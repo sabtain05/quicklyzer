565e72e1aba04f01ae6b0f7cf8016d033e4db177
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-01 12:07:41 +0500

    Chore: release 0.9.0

--- src/cli/banner.ts
+++ src/cli/banner.ts
@@ -8,7 +8,7 @@ export function showBanner(): void {
   console.log(chalk.cyan.bold("QUICKLYZER"));
   console.log(chalk.gray("===================================="));
 
-  console.log(chalk.green("v0.8.0"));
+  console.log(chalk.green("v0.9.0"));
   console.log(chalk.white("A fast & Intelligent CLI that understands and analyzes software projects in seconds."));
 
   console.log();
